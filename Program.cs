f02a3293fd8d77c5618f410fd1203faaafeb665a
ruancuan <610700249@qq.com>
2019-12-30 17:01:47 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,17 +12,36 @@ namespace test
     public class Program
     {
         const string apiAddress = @"http://openapi.tuling123.com/openapi/api/v2";
-        const string key="4e2802cbe401441093e95b1400c33ea1";
+        const string key="820c4a6ca4694063ab6002be1d1c63d3";
+        static Dictionary<int,string> errorDic=new Dictionary<int, string>();
 
         public static void Main(string[] args)
         {
-            JsonData jsonData=new JsonData();
-            jsonData["key"]=key;
-            jsonData["info"]="Hello";
-            jsonData["userid"]="213";
-            string inputString=JsonMapper.ToJson(jsonData);
+            InitDic();
+            string inputString="{'reqType':0,'perception':{'inputText':{'text':'Hello'}},'userInfo':{'apiKey':'820c4a6ca4694063ab6002be1d1c63d3','userId':'213'}}";
+            inputString.Split('\'','"');
             Console.WriteLine(GetResponse(inputString,1));
             Console.Read();
+        }
+
+        public static void InitDic(){
+            errorDic.Add(5000,"无解析结果");
+            errorDic.Add(6000,"暂不支持此功能");
+            errorDic.Add(4000,"请求参数格式错误");
+            errorDic.Add(4001,"加密方式错误");
+            errorDic.Add(4002,"无功能权限");
+            errorDic.Add(4003,"该apikey没有可用请求次数");
+            errorDic.Add(4005,"无功能权限");
+            errorDic.Add(4007,"apikey不合法");
+            errorDic.Add(4100,"userid获取失败");
+            errorDic.Add(4200,"上传格式错误");
+            errorDic.Add(4300,"批量操作超过限制");
+            errorDic.Add(4400,"没有上传合法userid");
+            errorDic.Add(4500,"userid申请个数超过限制");
+            errorDic.Add(4600,"输入内容为空");
+            errorDic.Add(4602,"输入文本内容超长(上限150)");
+            errorDic.Add(7002,"上传信息失败");
+            errorDic.Add(8008,"服务器错误");
         }
 
         public static string HttpGet(string url, Encoding encoding)
@@ -54,8 +73,6 @@ namespace test
         {
             if (url == null)
                 return "";
-            //string url = apiAddress+"?key=" + key + "&info=" + str+"&userid="+userId;
-
             Encoding encoding = Encoding.GetEncoding("utf-8");
             string data = HttpGet(url, encoding);
             Newtonsoft.Json.JsonReader reader = new JsonTextReader(new StringReader(data));
@@ -67,48 +84,15 @@ namespace test
                 {
                     reader.Read();
                     int excode = Convert.ToInt32(reader.Value);
-                    switch (excode)
-                    {
-                        case 100000:
+                    if(errorDic.ContainsKey(excode)){
+                        Console.WriteLine(errorDic[excode]);
+                    }
+                    else{
+                        if(excode==100000){
                             reader.Read();
                             reader.Read();
                             return reader.Value.ToString();
-                        case 5000:
-                            return "无解析结果";
-                        case 6000:
-                            return "暂不支持此功能";
-                        case 4000:
-                            return "请求参数格式错误";
-                        case 4001:
-                            return "加密方式错误";
-                        case 4002:
-                            return "无功能权限";
-                        case 4003:
-                            return "该apikey没有可用请求次数";
-                        case 4005:
-                            return "无功能权限";
-                        case 4007:
-                            return "apikey不合法";
-                        case 4100:
-                            return "userid获取失败";
-                        case 4200:
-                            return "上传格式错误";
-                        case 4300:
-                            return "批量操作超过限制";
-                        case 4400:
-                            return "没有上传合法userid";
-                        case 4500:
-                            return "userid申请个数超过限制";
-                        case 4600:
-                            return "输入内容为空";
-                        case 4602:
-                            return "输入文本内容超长(上限150)";
-                        case 7002:
-                            return "上传信息失败";
-                        case 8008:
-                            return "服务器错误";
-                        default:
-                            return "发生异常,异常码为:" + excode.ToString();
+                        }
                     }
                 }
             }
